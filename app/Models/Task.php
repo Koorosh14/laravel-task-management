0fac8810ddeb721d66c4e09822141c12821b9b4f
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-05-12 13:51:34 +0330

    Add relationships to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -11,4 +11,24 @@ class Task extends Model
 	use HasFactory;
 
 	protected $fillable = ['title', 'description', 'status', 'is_important', 'due_date', 'created_by', 'assigned_to', 'parent_id'];
+
+	public function creator()
+	{
+		return $this->belongsTo(User::class, 'created_by');
+	}
+
+	public function assignee()
+	{
+		return $this->belongsTo(User::class, 'assigned_to');
+	}
+
+	public function parent()
+	{
+		return $this->belongsTo(Task::class, 'parent_id');
+	}
+
+	public function subtasks()
+	{
+		return $this->hasMany(Task::class, 'parent_id');
+	}
 }
