6e6dde981028f48e5799e3af9a25bb8f7ff87b2b
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-05-21 22:21:38 +0330

    Add show method to TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,4 +15,17 @@ class TaskController extends Controller
 	{
 		return view('tasks.index', ['tasks' => Task::all()]);
 	}
+
+	/**
+	 * Displays a specific task.
+	 *
+	 * @param	int		$id
+	 *
+	 * @return	\Illuminate\Contracts\View\View
+	 */
+	public function show($id)
+	{
+		$task = Task::findOrFail($id); // Find the task or throw 404 if not found
+		return view('tasks.show', compact('task'));
+	}
 }
