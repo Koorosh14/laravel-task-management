764af5709381a95a55bc6c964b5abbded816dc3e
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-05-25 01:44:00 +0330

    Add Overdue badge to tasks in task index page

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -43,6 +43,10 @@
 									</svg>
 									{{ $task->due_date->format('M d, Y') }}
 								</div>
+
+								@if ($task->due_date->isPast() && $task->status !== \App\TaskStatus::COMPLETED)
+									<span class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-red-600/10 ring-inset">Overdue</span>
+								@endif
 							@endif
 						</div>
 					</div>
