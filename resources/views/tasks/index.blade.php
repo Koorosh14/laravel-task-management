00c4fa304787e891acdf372ba8d77acb69086221
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-05-25 01:47:40 +0330

    Display 'Assigned to' in tasks index page

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -48,6 +48,16 @@
 									<span class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-red-600/10 ring-inset">Overdue</span>
 								@endif
 							@endif
+
+							@if ($task->assignee)
+								<div class="flex items-center">
+									<svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+										<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+											d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+									</svg>
+									{{ $task->assignee->name }}
+								</div>
+							@endif
 						</div>
 					</div>
 				</div>
