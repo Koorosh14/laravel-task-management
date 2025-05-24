0ff81275a607d14cba657a06c0ed9c10a3479bc5
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-05-25 01:53:02 +0330

    Simplify TaskController's show() using Task model as parameter

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -19,13 +19,12 @@ class TaskController extends Controller
 	/**
 	 * Displays a specific task.
 	 *
-	 * @param	int		$id
+	 * @param	Task		$task
 	 *
 	 * @return	\Illuminate\Contracts\View\View
 	 */
-	public function show($id)
+	public function show(Task $task)
 	{
-		$task = Task::findOrFail($id); // Find the task or throw 404 if not found
 		return view('tasks.show', compact('task'));
 	}
 }
