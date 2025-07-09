226f2c18a47053fea897ca9f858f4a49c9400d7b
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-07-09 19:48:06 +0330

    Add edit button to tasks index page

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -111,6 +111,15 @@
 					</div>
 
 					<div class="d-flex gap-1">
+						<a href="{{ route('tasks.edit', $task->id) }}" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition ease-in-out duration-150">
+							<svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+									d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+								</path>
+							</svg>
+							Edit
+						</a>
+
 						<form action="{{ route('tasks.destroy', $task->id) }}" method="POST" class="inline-block">
 							@csrf
 							@method('DELETE')
