42e806a56622e5c24298fc64defd127812824146
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-07-11 02:09:21 +0330

    Move flash messages to a separate component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,23 +15,7 @@
 
 <body>
 	<main class="container my-4 mx-auto">
-		@if (session('success'))
-			<div id="flash-message-success" class="mb-4 bg-green-100 border-l-4 border-green-500 text-green-700 p-4 rounded shadow" role="alert">
-				{{ session('success') }}
-			</div>
-			<script>
-				setTimeout(() => { document.getElementById('flash-message-success').style.display = 'none'; }, 5000);
-			</script>
-		@endif
-
-		@if (session('error'))
-			<div id="flash-message-error" class="mb-4 bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded shadow" role="alert">
-				{{ session('error') }}
-			</div>
-			<script>
-				setTimeout(() => { document.getElementById('flash-message-error').style.display = 'none'; }, 5000);
-			</script>
-		@endif
+		<x-flash-message />
 
 		@yield('content')
 	</main>
