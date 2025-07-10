2daff6ebb427a6b3dd8661e1bda76e7e2f26a2ae
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-07-11 01:51:02 +0330

    Display success and error flash messages in app Blade template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,18 @@
 
 <body>
 	<main class="container my-4 mx-auto">
+		@if (session('success'))
+			<div id="flash-message-success" class="mb-4 bg-green-100 border-l-4 border-green-500 text-green-700 p-4 rounded shadow" role="alert">
+				{{ session('success') }}
+			</div>
+		@endif
+
+		@if (session('error'))
+			<div id="flash-message-error" class="mb-4 bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded shadow" role="alert">
+				{{ session('error') }}
+			</div>
+		@endif
+
 		@yield('content')
 	</main>
 
