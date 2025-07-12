321074d4974a33a865d49d01e927a199d0fde0de
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-07-13 01:11:16 +0330

    Add a simple navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,27 @@
 </head>
 
 <body>
+	<nav class="bg-blue-600 mb-4">
+		<div class="container mx-auto flex items-center justify-between py-3 px-4">
+			<a class="text-white font-bold text-xl" href="{{ url('/') }}">{{ config('app.name', 'Task Management') }}</a>
+			<div class="hidden lg:flex lg:items-center w-full lg:w-auto lg:space-x-4">
+				<ul class="flex flex-col lg:flex-row lg:space-x-4 w-full lg:w-auto">
+					<li>
+						<a class="block px-3 py-2 rounded text-white hover:bg-blue-700" href="{{ route('tasks.index') }}">Tasks</a>
+					</li>
+				</ul>
+				<ul class="flex flex-col lg:flex-row lg:space-x-4 mt-2 lg:mt-0">
+					<li>
+						<a class="block px-3 py-2 rounded text-white hover:bg-blue-700" href="#">Login</a>
+					</li>
+					<li>
+						<a class="block px-3 py-2 rounded text-white hover:bg-blue-700" href="#">Register</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
 	<main class="container my-4 mx-auto">
 		<x-flash-message type="success" class="bg-green-100 border-green-500 text-green-700" />
 		<x-flash-message type="error" class="bg-red-100 border-red-500 text-red-700" />
