a1a01eabb12012e6a432a488b650947232fdaf03
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2025-08-05 14:46:12 +0330

    Move all tasks routes under auth middleware

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('welcome'));
 
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::post('/tasks/create', [TaskController::class, 'store'])->name('tasks.store');
-Route::get('/tasks/edit/{task}', [TaskController::class, 'edit'])->name('tasks.edit');
-Route::put('/tasks/edit/{task}', [TaskController::class, 'update'])->name('tasks.update');
-Route::patch('/tasks/edit/{task}', [TaskController::class, 'updateStatus'])->name('tasks.update_status');
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+// Authenticated routes (only accessible when authenticated)
+Route::middleware('auth')->group(function()
+{
+	// Tasks routes
+	Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+	Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+	Route::post('/tasks/create', [TaskController::class, 'store'])->name('tasks.store');
+	Route::get('/tasks/edit/{task}', [TaskController::class, 'edit'])->name('tasks.edit');
+	Route::put('/tasks/edit/{task}', [TaskController::class, 'update'])->name('tasks.update');
+	Route::patch('/tasks/edit/{task}', [TaskController::class, 'updateStatus'])->name('tasks.update_status');
+	Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+	Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+});
 
 require base_path('routes/auth-routes.php');
